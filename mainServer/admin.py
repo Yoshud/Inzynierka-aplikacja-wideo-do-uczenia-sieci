e5e21fbee886fb003151d4c875819a76299abe25
Yoshud <adam0cisak@gmail.com>
2018-07-07 12:10:57 +0200

    Zmiany w modelach dodajace informacje o sesji i daty, dodanie widoku admina i podpiecie aplikacji w settings
    (cherry picked from commit 25c88b7)

--- mainServer/admin.py
+++ mainServer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from mainServer import models
+from django.db.models.base import ModelBase
 
-# Register your models here.
+abstracts = {"Status"}
+for model_name in dir(models):
+    model = getattr(models, model_name)
+    if (model_name not in abstracts) and isinstance(model, ModelBase):
+        admin.site.register(model)
+
+
--- mainServer/models.py
+++ mainServer/models.py
@@ -8,13 +8,6 @@ class Status(models.Model):
         abstract = True
 
 
-class Folder(models.Model):
-    sciezka = models.TextField(default="")
-
-    class Meta:
-        abstract = True
-
-
 class StatusFilmu(Status):
     pass
 
@@ -36,39 +29,35 @@ class Klatka(models.Model):
 
 
 class Film(models.Model):
-    sciezka = models.TextField(default="", blank=True, null=True)
-    nazwa = models.CharField("nazwa", max_length=80, default="", blank=True, null=True)
+    sciezka = models.TextField(blank=True, null=True)
+    nazwa = models.CharField("nazwa", max_length=80, blank=True, null=True)
     FPS = models.FloatField("fps", default=0.0)
     dlugosc = models.TimeField("dlugosc", blank=True, null=True)
     rozmiarX = models.IntegerField("x", default=0)
     rozmiarY = models.IntegerField("y", default=0)
     iloscKlatek = models.IntegerField("iloscKlatek", blank=True, null=True)
-    # folder = models.ForeignKey("FolderFilmy", on_delete=models.CASCADE)
-    status = models.ManyToManyField("StatusFilmu", blank=True, null=True)
+    data = models.DateTimeField(default=timezone.now)
+    status = models.ManyToManyField("StatusFilmu", blank=True)
     sesja = models.ForeignKey("Sesja", on_delete=models.CASCADE)
 
 
-class FolderFilmy(Folder):
-    sesja = models.ForeignKey("Sesja", on_delete=models.CASCADE)
-
-
-class FolderZObrazami(Folder):
-    pass
+class FolderZObrazami(models.Model):
+    sciezka = models.TextField(default="")
 
 
 class Obraz(models.Model):
     klatka = models.OneToOneField("Klatka", blank=True, null=True, on_delete=models.CASCADE)
-    folder = models.ForeignKey("FolderFilmy", on_delete=models.CASCADE)
+    folder = models.ForeignKey("FolderZObrazami", on_delete=models.CASCADE)
     nazwa = models.CharField("nazwa", max_length=80, default="")
-
+    data = models.DateTimeField(default=timezone.now)
 
 # Konieć CZ1:
 
 class ZbioryDanych(models.Model):
     sesja = models.ForeignKey("Sesja", on_delete=models.CASCADE)
-    uczacy = models.ManyToManyField("Obraz")
-    walidacyjny = models.ManyToManyField("Obraz", blank=True, null=True)
-    testowy = models.ManyToManyField("Obraz", blank=True, null=True)
+    uczacy = models.ManyToManyField("Obraz", related_name="zbioryUczacy")
+    walidacyjny = models.ManyToManyField("Obraz", related_name="zbioryWalidacyjny", blank=True)
+    testowy = models.ManyToManyField("Obraz", related_name="zbioryTestowy", blank=True)
 
 
 class Sieci(models.Model):
@@ -86,7 +75,7 @@ class ParametryUczenia(models.Model):
 
 
 class AktualneWyniki(models.Model):  # używane przez CRONa głównego backendu do śledzenia uczenia sieci bez potrzeby kounikacji
-    czas = models.DateTimeField(default=timezone.now())
+    data = models.DateTimeField(default=timezone.now)
     status = models.TextField(default="")
 
 
@@ -125,5 +114,8 @@ class WynikPrzetwarzania(models.Model):
 
 class Sesja(models.Model):
     folderZObrazami = models.OneToOneField("FolderZObrazami", on_delete=models.CASCADE)
-    filmyPrzetworzone = models.ManyToManyField("Film", blank=True, null=True)
-    filmyUzytkownik = models.ManyToManyField("Film", blank=True, null=True)
+    id = models.AutoField(primary_key=True)
+    filmyPrzetworzone = models.ManyToManyField("Film", related_name="sesjaPrzetworzone", blank=True)
+    filmyUzytkownik = models.ManyToManyField("Film", related_name="sesjaUzytkownika", blank=True)
+    data = models.DateTimeField(default=timezone.now)
+    nazwa = models.TextField(default="Nienazwana_{}__{}".format(id, timezone.now()))
